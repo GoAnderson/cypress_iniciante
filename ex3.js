0224e1e86dd5982b7dadb798a0fc0de50c503e48
GoAnderson <goandersonguima@gmail.com>
2025-03-27 16:44:51 -0300

    Enviando tudo

---
+++ ex3.js
@@ -0,0 +1,6 @@
+function somarNumero(num1, num2){
+    const soma = num1 + num2
+    console.log('A soma eh: ' + soma)
+}
+
+somarNumero(10,50)
---
+++ funcoes2.js
@@ -0,0 +1,12 @@
+//Funcoes
+
+function gerarNome(nome1, sobrenome){
+    return 'Nome:' + nome1 + sobrenome
+
+}
+
+// gerarNome()
+// console.log(gerarNome('Luiz ', 'Silva'))
+
+const nomeGerado = gerarNome('Pedro ', 'Tuz')
+console.log(nomeGerado)
