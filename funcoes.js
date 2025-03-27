6b64ef530393b1608202eb2f6667265bb36b3ea4
GoAnderson <goandersonguima@gmail.com>
2025-03-27 09:04:18 -0300

    Enviando o primeiro code de funcoes

---
+++ funcoes.js
@@ -0,0 +1,13 @@
+//Funcoes
+
+function gerarNome(nome1, sobrenome, idade){
+    // const nome = 'Anderson'
+    // const sobrenome = ' Guima'
+    // const idade = 30
+
+    console.log('Nome:', nome1 + sobrenome)
+    console.log('Idade: ', idade)
+}
+
+// gerarNome()
+gerarNome('Luiz', 'Silva', 18)
